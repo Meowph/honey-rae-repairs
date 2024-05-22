12853960152aa583e550b286ab9216bb2e2817d9
Meowph <bpardue33@gmail.com>
2024-05-22 15:24:57 -0400

    ch 1 in part 2

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react"
+import "./tickets/Tickets.css"
+import { getAllTickets } from "../services/ticketService.jsx"
+import { Ticket } from "./tickets/Ticket.jsx"
+
+
+// <> </> is a react fragment which wraps the other elements. JSX only allows for one parent element
+export const TicketList = () => {
+  const [allTickets, setAllTickets] = useState([])
+  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
+  const [filteredTickets, setFilteredTickets] = useState([]) //leaving this as empty () shows error in console
+
+  //Two Arguments: function & array (dependency array)... Function = WHAT we want to happen... Array = WHEN we want it to happen
+  useEffect(() => {
+    getAllTickets().then(ticketsArray => {
+      setAllTickets(ticketsArray)
+      console.log("tickets set!")
+    })
+  }, []) //Empty dependency array? ONLY runs function on initial render of component
+
+  useEffect(() => {
+    if(showEmergencyOnly) {
+      const emergencyTickets = allTickets.filter(ticket => ticket.emergency === true)
+      setFilteredTickets(emergencyTickets)
+
+    } else {
+      setFilteredTickets(allTickets)
+    }
+  }, [showEmergencyOnly, allTickets])
+
+  return ( 
+      <div className="tickets-container">
+        <h2>Tickets</h2>
+        <div>
+          <button className="filter-btn btn-primary" 
+          onClick={ () => {setShowEmergencyOnly(true)}}>
+            Emergency
+            </button>
+          <button className="filter-btn btn-info"
+        onClick={() => {setShowEmergencyOnly(false)}}>
+          Show All
+        </button>
+        </div>
+        <article className="tickets">
+          {filteredTickets.map((ticketObj) => {
+            return <Ticket ticket={ticketObj} name="Joe" key= {ticketObj.id}/>
+          })}
+        </article>
+      </div>
+  )
+}
+
+//Setter function from useState is used instead of counter++ because it tells React to re-render the info. It tells the computer that the state changed and the HTM needs to be re-rendered
+
+//JS info/expression = info in { }
+
+//ticket=(ticketObj) is a prop with a key value pair for the component of Ticket, which is being rendered from Ticket.jsx (anytime a prop is passed onto a component a kye value pair is created on props obj for component)
---
+++ src/components/tickets/Ticket.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+import { getAllEmployees } from "../../services/employeeService.jsx"
+
+export const Ticket = ({ticket, name}) => {
+    const [employees, setEmployees] = useState ([])
+    const [assignedEmployee, setAssignedEmployee] = useState({})
+
+    //invoke getAllEmployees function in order to get the employees form the database. 
+    //Then, set employees state with the employeeArray that came back from database
+    useEffect(() => {
+      getAllEmployees().then((employeeArray) => {
+        setEmployees(employeeArray)
+      })
+    }, [])
+
+    // Watch for when employees state changes 
+    // By adding a ? after [0], it runs ticket, then .employeeTickets. If it comes back undefined, meaning employee.id would === undefined instead of employeeId. The ? stops it right after the [0] so the employeeId property is accessed
+    //If employee is found, it will become an object, if not, it will come back as undefined
+    // Using ? is best when you expect to get undefined back. IF you don't, but that is what shows, don't just throw a chaining operator to "fix" the problem
+    useEffect(() => {
+      const foundEmployee = employees.find(
+        (employee) => employee.id === ticket.employeeTickets[0]?.employeeId)
+        setAssignedEmployee(foundEmployee)
+    }, [employees, ticket])
+
+  return (
+  <section className="ticket" >
+  <header className="ticket-info">#{ticket.id}</header>
+  <div>{ticket.description}</div>
+  <footer>
+    <div>
+      <div className="ticket-info">assignee</div>
+      <div>{assignedEmployee ? assignedEmployee.user?.fullName : "None"}</div>
+    </div>
+    <div>
+      <div className="ticket-info" key={ticket.id}>Emergency</div>
+      <div>{ticket.emergency ? "yes" : "no"}</div>
+    </div>
+  </footer>
+</section>
+  )
+}
